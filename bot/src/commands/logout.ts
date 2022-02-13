6f1d2db2f524828399631b6e01070f890ea3b55e
James <jmsdevln@gmail.com>
2022-02-12 22:47:39 -0800

    attempted logout oreid service call

--- bot/src/commands/logout.ts
+++ bot/src/commands/logout.ts
@@ -1,11 +1,27 @@
-import { SlashCommandBuilder } from "@discordjs/builders";
+import { SlashCommandBuilder } from "@discordjs/builders"
 import { MessageEmbed } from "discord.js"
 import { CommandInt } from "../interfaces/CommandInt"
 import { errorHandler } from "../utils/errorHandler"
 import { checkLoggedIn, setDiscordUserState } from "../modules/mongo"
 import { logHandler } from "../utils/logHandler"
-import { set } from "mongoose";
-import { login } from "./login";
+import axios from "axios"
+import { RequestMethod } from "@discordjs/rest"
+
+// async function logoutOreId() {
+//     try {
+//         const response = await axios.get(process.env.OREID_URL + '/logout', {
+//             params: {
+//                 app_id: process.env.OREID_APP_ID,
+//                 providers: "all",
+//                 callback_url: process.env.OREID_AUTH_CALLBACK_URL
+//             }
+//         })
+//         console.log(response);
+//     } 
+//     catch (error) {
+//         console.error(error);
+//     }
+// }
 
 export const logout: CommandInt = {
     data: new SlashCommandBuilder()
@@ -16,15 +32,27 @@ export const logout: CommandInt = {
         // Logout from OreId
         // 
 
-        // Check if user is already logged in
+        // Check if user is already logged in and lastLogin Date
         let userCheck = await checkLoggedIn(Number(interaction.user.id))
         .then(async function(response: [boolean, string]) {
-            logHandler.info("userCheck: " + response)
+            logHandler.info("checkLoggedIn: " + response)
+
+            // Create reply object that only the user can see
             await interaction.deferReply({ ephemeral: true })
+            
+            // Begin the message embed object
             const loginEmbed = new MessageEmbed()
             loginEmbed.setThumbnail('https://i.imgur.com/A3yS9pl.png')
+
+            // If logged in
             if (response[0] == true) {
                 try {
+                    // call the api /logout endpoint to fully
+                    // logout the user from ORE-ID
+                    // await logoutOreId()
+
+                    // Reset the Discord Users State to "None" and
+                    // set the loggedIn status to false
                     await setDiscordUserState(
                         Number(interaction.user.id),
                         "None",
@@ -38,6 +66,7 @@ export const logout: CommandInt = {
                 errorHandler("logout command failed: ", err)
                 }
             }
+            // else if not logged in
             else {
                 loginEmbed.setTitle("⭐️ Surprise")
                 loginEmbed.setDescription(
